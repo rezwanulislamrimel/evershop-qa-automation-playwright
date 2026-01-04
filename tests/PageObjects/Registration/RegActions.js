37bcc18c91da81d39f643edf4ff766be67cb0966
Rimel <rezwanulrimels@gmail.com>
2026-01-05 03:04:58 +0600

    registration action added

--- tests/PageObjects/Registration/RegActions.js
+++ tests/PageObjects/Registration/RegActions.js
@@ -4,4 +4,24 @@ export class Registraiton {
     this.page = page;
     this.locator = new RegistrationObjects(page);
   }
+
+  async ClickOnAccountIcon() {
+    await this.locator.AccountIcon.Click();
+  }
+  async ClickOnRegistrationIcon() {
+    await this.locator.CreateAccountLink.Click();
+  }
+
+  async EnterFullName() {
+    await this.locator.emailInput.fill("rezwanul");
+  }
+  async EnterEmail() {
+    await this.locator.emailInput.fill("rezwanul@gmail.com");
+  }
+  async EnterPassword() {
+    await this.locator.passwordInput.fill("password123");
+  }
+  async ClickSignUpButton() {
+    await this.locator.buttonSignUp.click();
+  }
 }
--- tests/PageObjects/Registration/RegObjects.js
+++ tests/PageObjects/Registration/RegObjects.js
@@ -6,8 +6,8 @@ export class RegistrationObjects {
         name: register,
       }));
     this.fullNameInput = this.page.getByRole("text", { name: "full_name" });
-    this.email = this.page.getByRole("text", { name: "Email" });
-    this.password = this.page.getByRole("text", { name: "Password" });
+    this.emailInput = this.page.getByRole("text", { name: "Email" });
+    this.passwordInput = this.page.getByRole("text", { name: "Password" });
     this.button = this.page.getByRole("button", { name: "Sign Up" });
   }
 }
